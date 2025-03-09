d3b584f0436ee6dd508575fa61e7f5946021e364
Grégoire Naisse <gregouar08@gmail.com>
2025-03-10 00:01:08 +0100

    cors fix

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,21 @@
 use axum::{response::Json, routing::get, Router};
 use serde_json::{json, Value};
 
+use http::Method;
+use tower::ServiceBuilder;
+use tower_http::cors::{Any, CorsLayer};
+
 #[tokio::main]
 async fn main() {
+    let cors_layer = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods([Method::GET, Method::POST]);
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/pou", get(get_test))
-        .route("/jsonpou", get(get_json));
+        .route("/jsonpou", get(get_json))
+        .layer(ServiceBuilder::new().layer(cors_layer));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4200").await.unwrap();
     axum::serve(listener, app).await.unwrap();
