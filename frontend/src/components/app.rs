57233e563b26e1e5eab339822b73183b1114b548
Grégoire Naisse <gregouar08@gmail.com>
2025-03-28 21:40:08 +0100

    base uri

--- frontend/src/components/app.rs
+++ frontend/src/components/app.rs
@@ -11,26 +11,15 @@ use leptos_router::path;
 pub fn App() -> impl IntoView {
     provide_meta_context();
 
-    // let base_uri = move || {
-    //     window()
-    //         .document()
-    //         .map(|doc| doc.base_uri().unwrap_or_default())
-    //         .unwrap_or_else(|| Some("/".to_string())) // Default to root
-    // };
-
-    // let base_uri = move || document().base_uri().unwrap().unwrap_or("/".to_string());
-
     let base_uri: Cow<'static, str> = document()
         .base_uri()
         .unwrap_or(Some("/".to_string()))
         .unwrap_or("/".to_string())
         .into();
 
-    println!("{}", base_uri);
-
     view! {
-        <Router base="/">
-            <Routes fallback=|| base_uri>
+        <Router base=base_uri>
+            <Routes fallback=|| "Page not found.">
                 <Route path=path!("/") view=MainMenu/>
                 <Route path=path!("/game") view=Game/>
             </Routes>
