1f6ddf595785ff127c608feffdc65981fa8a794f
NAG <nag@skeyes.be>
2025-04-23 23:03:21 +0200

    again

--- frontend/src/components/game/header_menu.rs
+++ frontend/src/components/game/header_menu.rs
@@ -27,34 +27,30 @@ pub fn HeaderMenu() -> impl IntoView {
     view! {
         <div class="relative z-50 flex justify-between items-center p-2 bg-zinc-800 shadow-md h-auto">
             <div class="flex justify-around w-full items-center">
-                // <div class="text-shadow-md shadow-gray-950 text-amber-200 text-xl flex justify-between">
-                // <div>
-                // <span>"Gold: "</span>
+                <div class="text-shadow-md shadow-gray-950 text-amber-200 text-xl flex justify-between items-center">
+                    <div>
+                        <span>"Gold: "</span>
+                    </div>
+                    <div class="flex items-center space-x-1">
+                        <Number value=gold />
+                        // <div>
+                        <img src=img_asset("ui/gold.webp") alt="gold_icon" class="object-contain" />
+                    // </div>
+                    </div>
+                </div>
+                // <div class="relative flex-1 h-full">
+                // <div class="absolute left-0 top-1/2 -translate-y-1/2 text-shadow-md shadow-gray-950 text-amber-200 text-xl">
+                // Gold:
                 // </div>
-                // <div class="flex-1 flex items-center space-x-1">
+                // <div class="absolute right-0 top-1/2 -translate-y-1/2 flex items-center space-x-1 text-amber-200 text-xl">
                 // <Number value=gold />
-                // <div class="h-full w-auto ">
                 // <img
                 // src=img_asset("ui/gold.webp")
                 // alt="gold_icon"
-                // class="object-cover aspect-square"
+                // class="h-full w-auto object-contain"
                 // />
                 // </div>
                 // </div>
-                // </div>
-                <div class="relative flex-1 h-full">
-                    <div class="absolute left-0 top-1/2 -translate-y-1/2 text-shadow-md shadow-gray-950 text-amber-200 text-xl">
-                        Gold:
-                    </div>
-                    <div class="absolute right-0 top-1/2 -translate-y-1/2 flex items-center space-x-1 text-amber-200 text-xl">
-                        <Number value=gold />
-                        <img
-                            src=img_asset("ui/gold.webp")
-                            alt="gold_icon"
-                            class="h-full w-auto object-contain"
-                        />
-                    </div>
-                </div>
 
                 <div>
                     <p class="text-shadow-md shadow-gray-950 text-xl">"Magic Essence: 0"</p>
