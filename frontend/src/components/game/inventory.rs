7ff9178a7431ee6e67d1f107da2c73a3ef02cdea
Grégoire Naisse <gregouar08@gmail.com>
2025-05-04 21:40:50 +0200

    toward tooltip switch on equip

--- frontend/src/components/game/inventory.rs
+++ frontend/src/components/game/inventory.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
 
 use leptos::html::*;
+use leptos::logging;
 use leptos::prelude::*;
+use leptos_use::use_mouse;
 
 use shared::data::item::{
     AffixEffect, AffixEffectType, ItemCategory, ItemRarity, ItemSpecs, ItemStat,
@@ -177,8 +179,22 @@ fn ItemCard(item_specs: ItemSpecs) -> impl IntoView {
     let icon_asset = img_asset(&item_specs.icon);
 
     let tooltip_context = expect_context::<DynamicTooltipContext>();
+    let rc_item_specs = Arc::new(item_specs.clone());
+    let rc_item_specs2 = rc_item_specs.clone();
     let show_tooltip = move |_| {
-        let item_specs = Arc::new(item_specs.clone());
+        let item_specs = rc_item_specs.clone();
+        tooltip_context.set_content(
+            move || {
+                let item_specs = item_specs.clone();
+                view! { <ItemTooltip item_specs=item_specs /> }.into_any()
+            },
+            DynamicTooltipPosition::BottomRight,
+        );
+    };
+
+    let tooltip_context = expect_context::<DynamicTooltipContext>();
+    let show_tooltip2 = move |_| {
+        let item_specs = rc_item_specs2.clone();
         tooltip_context.set_content(
             move || {
                 let item_specs = item_specs.clone();
@@ -190,9 +206,42 @@ fn ItemCard(item_specs: ItemSpecs) -> impl IntoView {
 
     let tooltip_context = expect_context::<DynamicTooltipContext>();
     let hide_tooltip = move |_| tooltip_context.hide();
+    let tooltip_context = expect_context::<DynamicTooltipContext>();
+    let hide_tooltip2 = move |_| tooltip_context.hide();
+
+    let el_ref = NodeRef::new();
+
+    let mouse = use_mouse();
+    let mouse_ref = mouse.x.get();
+
+    let mouse = use_mouse(); // TODO: How to retrieve last known position without being registered for tracking?
+    let on_mount = move |el: NodeRef<Div>| {
+        let el = el.get().unwrap();
+        let rect = el.get_bounding_client_rect();
+        let (x, y) = (mouse.x.get_untracked(), mouse.y.get_untracked());
+
+        logging::error!("{},{}", mouse_ref, mouse_ref);
+        logging::error!("{},{}", x, y);
+        logging::error!(
+            "{},{},{},{}",
+            rect.left(),
+            rect.right(),
+            rect.top(),
+            rect.bottom()
+        );
+
+        if x >= rect.left() && x <= rect.right() && y >= rect.top() && y <= rect.bottom() {
+            show_tooltip2(());
+        };
+    };
+
+    Effect::new(move || {
+        on_mount(el_ref);
+    });
 
     view! {
         <div
+            node_ref=el_ref
             class=format!(
                 "relative group flex items-center justify-center w-full h-full
                 rounded-md p-1 bg-gradient-to-br {} border-4 {} ring-2 {} shadow-md {}",
@@ -203,10 +252,14 @@ fn ItemCard(item_specs: ItemSpecs) -> impl IntoView {
             )
             on:mouseenter=show_tooltip
             on:mouseleave=hide_tooltip
+            on:click=hide_tooltip2
         >
             <img
                 src=icon_asset
-                class="object-contain max-w-full max-h-full transition-all duration-150 ease-in-out group-hover:scale-105 group-hover:brightness-110"
+                class="object-contain max-w-full max-h-full transition-all duration-50 ease-in-out
+                group-hover:scale-105 group-hover:brightness-110
+                group-active:scale-90 group-active:brightness-90
+                "
             />
         </div>
     }
