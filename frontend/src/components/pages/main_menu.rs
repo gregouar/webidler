6274e5106efe750b1d902bf32fbfa9d087b1635e
NAG <nag@skeyes.be>
2025-05-13 00:26:22 +0200

    around

--- frontend/src/components/pages/main_menu.rs
+++ frontend/src/components/pages/main_menu.rs
@@ -51,7 +51,7 @@ pub fn MainMenu() -> impl IntoView {
     };
 
     view! {
-        <main class="my-0 mx-auto max-w-3xl text-center flex flex-col justify-between">
+        <main class="my-0 mx-auto max-w-3xl text-center flex flex-col justify-around">
             <h1 class="text-shadow-lg shadow-gray-950 mb-4 text-amber-200 text-4xl  md:text-5xl lg:text-6xl font-extrabold leading-none tracking-tight">
                 "Welcome to Webidler!"
             </h1>
