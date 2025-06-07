613e6d929e23cb955e93db8e6b99b3216ebe75a0
Grégoire Naisse <gregouar08@gmail.com>
2025-06-07 21:38:15 +0200

    pffff

--- backend/src/db/utc_datetime.rs
+++ backend/src/db/utc_datetime.rs
@@ -1,4 +1,8 @@
+use sqlx::decode::Decode;
+use sqlx::sqlite::{Sqlite, SqliteValueRef};
 use sqlx::types::chrono::{DateTime, NaiveDateTime, Utc};
+use sqlx::types::Type;
+use std::error::Error;
 
 #[derive(Debug, Clone)]
 pub struct UtcDateTime(DateTime<Utc>);
@@ -18,5 +22,20 @@ impl From<DateTime<Utc>> for UtcDateTime {
 impl From<UtcDateTime> for DateTime<Utc> {
     fn from(dt: UtcDateTime) -> Self {
         dt.0
+    }
+}
+
+impl<'r> Decode<'r, Sqlite> for UtcDateTime {
+    fn decode(value: SqliteValueRef<'r>) -> Result<Self, Box<dyn Error + Send + Sync>> {
+        let naive = <NaiveDateTime as Decode<Sqlite>>::decode(value)?;
+        Ok(UtcDateTime(DateTime::<Utc>::from_naive_utc_and_offset(
+            naive, Utc,
+        )))
+    }
+}
+
+impl Type<Sqlite> for UtcDateTime {
+    fn type_info() -> <Sqlite as sqlx::Database>::TypeInfo {
+        <NaiveDateTime as Type<Sqlite>>::type_info()
     }
 }
