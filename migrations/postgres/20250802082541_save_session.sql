2a673fd14f24fc94f5c050ae499cbc35a329933d
Grégoire Naisse <gregouar08@gmail.com>
2025-08-02 13:58:32 +0200

    i should do proper ci

--- migrations/postgres/20250802082541_save_session.sql
+++ migrations/postgres/20250802082541_save_session.sql
@@ -1,5 +1,5 @@
 CREATE TABLE saved_game_instances (
     user_id VARCHAR(200) NOT NULL PRIMARY KEY,
     saved_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    game_data BLOB NOT NULL
+    game_data bytea NOT NULL
 );
