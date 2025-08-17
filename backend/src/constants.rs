7719fcb0e9b776c0e570f5a4a1a26312476a4bc5
Grégoire Naisse <gregouar08@gmail.com>
2025-08-17 16:45:36 +0200

    arf

--- backend/src/constants.rs
+++ backend/src/constants.rs
@@ -4,8 +4,8 @@ pub const CHARACTER_DATA_VERSION: &str = "0.1.0";
 
 // TODO: put in some game config file
 pub const DEFAULT_MAX_CHARACTERS: u8 = 5;
-pub const SKILL_BASE_COST: f64 = 1.0;
-pub const SKILL_COST_FACTOR: f64 = 1.0;
+pub const SKILL_BASE_COST: f64 = 100.0;
+pub const SKILL_COST_FACTOR: f64 = 1_000.0;
 pub const CHAMPION_BASE_CHANCES: f64 = 0.0001;
 pub const CHAMPION_INC_CHANCES: f64 = 0.00001;
 pub const CHAMPION_LEVEL_INC: AreaLevel = 5;
