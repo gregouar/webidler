92152cf057c10a4a7b4df6f8f42998c7e355f6e9
Grégoire Naisse <gregouar08@gmail.com>
2025-09-04 14:50:44 +0200

    Fix: better uc on usernames and characternames

---
+++ migrations/postgres/20250904124313_uc_inames.sql
@@ -0,0 +1,17 @@
+
+ALTER TABLE
+    users DROP CONSTRAINT users_username_key;
+
+CREATE UNIQUE INDEX uc_users_username_ci ON characters (LOWER(username))
+WHERE
+    deleted_at IS NULL;
+    
+
+ALTER TABLE
+    characters DROP CONSTRAINT uc_characters_character_name;
+
+CREATE UNIQUE INDEX uc_characters_character_name_ci ON characters (LOWER(character_name))
+WHERE
+    deleted_at IS NULL;
+
+    
