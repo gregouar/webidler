d70f97b99655638397dfc2d0e15338db9bec18d4
nag <nag@skeyes.be>
2026-01-24 20:25:33 +0100

    better

--- frontend/src/components/ui/card.rs
+++ frontend/src/components/ui/card.rs
@@ -11,31 +11,20 @@ pub fn Card(
 ) -> impl IntoView {
     view! {
         <div class=format!(
-            "max-h-full flex flex-col
-            bg-zinc-800 rounded-md shadow-xl/30
-            relative
-            border-1 
-            border-b-zinc-950 border-r-zinc-900
-            border-t-zinc-600 border-l-zinc-700
+            "max-h-full flex flex-col relative
+            bg-zinc-800 
+            rounded-[6px] xl:rounded-[8px]
+                 
+            ring-1 ring-zinc-900/80
+            shadow-xl/30
+
+            shadow-[inset_1px_1px_1px_rgba(255,255,255,0.06)]
+            shadow-[inset_-2px_-2px_3px_rgba(0,0,0,0.45)]
             {} {} {}",
             class.unwrap_or_default(),
             if gap { "gap-1 xl:gap-2" } else { "" },
             if pad { "p-1 xl:p-3" } else { "" },
-        )>
-            // <div class="
-            // absolute inset-0 rounded-md pointer-events-none
-            // ring-1 ring-white/10
-            // translate-x-[0.5px] translate-y-[0.5px]
-            // "></div>
-
-            // <div class="
-            // absolute inset-0 rounded-md pointer-events-none
-            // ring-1 ring-black/40
-            // -translate-x-[0.5px] -translate-y-[0.5px]
-            // "></div>
-
-            {children()}
-        </div>
+        )>{children()}</div>
     }
 }
 
@@ -75,6 +64,9 @@ pub fn CardInset(
         <div class=format!(
             "flex flex-col
             bg-neutral-900 shadow-[inset_0_0_32px_rgba(0,0,0,0.6)]
+            
+    shadow-[inset_-1px_-1px_2px_rgba(255,255,255,0.04)]
+    shadow-[inset_3px_3px_6px_rgba(0,0,0,0.6)]
             ring-1  ring-zinc-950
             overflow-y-auto
             {} {} {}",
