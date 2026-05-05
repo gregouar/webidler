a82288a80fb78c27b221b39189deec73a1b3f50e
nag <nag@skeyes.be>
2026-05-05 22:30:25 +0200

    mmm

--- frontend/src/components/chat/chat_context.rs
+++ frontend/src/components/chat/chat_context.rs
@@ -196,7 +196,7 @@ fn handle_message(chat_context: &ChatContext, message: ServerChatMessage) -> Con
     match message {
         ServerChatMessage::Connect(m) => {
             chat_context.user_id.set(Some(m.user_id));
-            for message in m.history {
+            for message in m.history.into_iter().rev() {
                 push_message(chat_context, message)
             }
         }
