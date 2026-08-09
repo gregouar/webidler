c8d9a5d4ab22685b1be7d75532dcb18ecb89f11e
nag <nag@skeyes.be>
2026-08-09 18:52:17 +0200

    oops

--- migrations/postgres/20260617184242_skill_mastery.sql
+++ migrations/postgres/20260617184242_skill_mastery.sql
@@ -1,9 +1,2 @@
-ALTER TABLE
-    characters
-ADD
-    COLUMN stamina DOUBLE PRECISION NOT NULL DEFAULT 0;
-
-ALTER TABLE
-    characters_data
-ADD
-    COLUMN skill_masteries_data BYTEA;
+ALTER TABLE characters_data
+ADD COLUMN skill_masteries_data BYTEA;
